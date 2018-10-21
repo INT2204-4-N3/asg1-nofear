b4aa9b0e2d93b2cbb7cd9b36962638c37c88b1af
NguyenAnhTuan1999 <43202476+NguyenAnhTuan1999@users.noreply.github.com>
2018-10-21 08:46:23 +0700

    Add files via upload

---
+++ MyDict/src/mydictionary/Translator.java
@@ -0,0 +1,40 @@
+package mydictionary;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class Translator {
+    public static String langFrom = "vi", langTo = "en";
+
+    public static void main(String[] args) throws IOException {
+        langFrom = "vi";
+        langTo = "en";
+        String text = "xin chào";
+        //Translated text:
+        System.out.println("Translated text: " + translate( text));
+    }
+
+    public static String translate(String text) throws IOException {
+        // INSERT YOU URL HERE
+        String urlStr = "https://script.google.com/macros/s/AKfycbzulZP1k9JYQ5B1k8vxOAmHIt-0COtCmz_Fa2x9ZiTBxvBaYeot/exec" +
+                "?q=" + URLEncoder.encode(text, "UTF-8") +
+                "&target=" + langTo +
+                "&source=" + langFrom;
+        URL url = new URL(urlStr);
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        return response.toString();
+    }
+
+}
